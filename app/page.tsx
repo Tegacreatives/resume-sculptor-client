8adb224963ca1ba2f27ecead9e2b15def0223a68
Tegacreatives <tegaokorare91@gmail.com>
2024-09-16 18:47:42 +0100

    add icon to top hero text

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import Button from "./components/Button";
 
+import { SiReaddotcv } from "react-icons/si";
+
 export default function Home() {
   return (
     <div>
       <section className="text-center space-y-10 pt-14 min-h-[100vh] lg:min-h-[80vh]">
-        <p className="text-gray-400 text-sm">Tailored Resume</p>
+        <div className="flex items-center justify-center space-x-2">
+          <p className="text-gray-400 text-sm">Tailored Resume</p>
+          <SiReaddotcv color="#9ca3af" />
+        </div>
         <h1 className="text-4xl font-semibold text-gray-800 capitalize">
           Easily Create tailored resumes for each <br /> job application based
           on the job description
