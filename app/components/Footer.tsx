8852d983db455ddaceb64e943eb0f15547d30feb
Tegacreatives <tegaokorare91@gmail.com>
2024-09-16 20:14:37 +0100

    Update footer text

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,17 @@
+import Link from "next/link";
 import React from "react";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div className="text-center text-sm text-gray-600 py-10 lg:py-0 px-10 md:px-14">
-      Hi I&apos;m Tega, currently working on bringing Resume Sculptor to life.{" "}
-      <br /> Follow me on X to see my progress on the project.
+    <div className="flex flex-col items-center justify-center text-sm text-gray-600 py-10 lg:py-0 px-10 md:px-14 space-y-2 lg:pb-10">
+      <p className="text-center">
+        Hi I&apos;m Tega, currently working on bringing Resume Sculptor to life.{" "}
+        <br /> Follow me on X to see my progress on the project.
+      </p>
+      <Link href="">
+        <FaXTwitter />
+      </Link>
     </div>
   );
 };
