c4f9eb16b909b71ee8002db1ed768ca183ad2563
Tegacreatives <tegaokorare91@gmail.com>
2024-09-16 19:05:28 +0100

    add padding to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="text-center text-sm text-gray-600 py-10 lg:py-0">
+    <div className="text-center text-sm text-gray-600 py-10 lg:py-0 px-10 md:px-14">
       Hi I&apos;m Tega, currently working on bringing Resume Sculptor to life.{" "}
       <br /> Follow me on X to see my progress on the project.
     </div>
