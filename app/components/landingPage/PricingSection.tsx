9326a9242cc1085a4b557111f0ba845c2ca07357
Tegacreatives <tegaokorare91@gmail.com>
2024-09-20 14:51:25 +0100

    Start pricing section

--- app/components/landingPage/PricingSection.tsx
+++ app/components/landingPage/PricingSection.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const PricingSection = () => {
-  return <div>PricingSection</div>;
+  return (
+    <section className="flex items-center justify-center text-center space-y-14">
+      <div className="space-y-3">
+        <h2>Affordable Pricing Plans</h2>
+        <p>
+          Create professional resumes with ease. Choose the bundle that best
+          fits your needs.
+        </p>
+      </div>
+      <div></div>
+    </section>
+  );
 };
 
 export default PricingSection;
